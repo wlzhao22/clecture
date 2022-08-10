9f7719a61ead4c740efe673415318d0a259e92e0
Einestein <106955055+wlzhao22@users.noreply.github.com>
2022-08-10 10:03:11 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,33 @@
+
+%sed -i -e 's/2022/2023/g' */*lec*.tex
+
+cd lecture1/
+pdflatex lec1.tex
+
+cd ../lecture2
+pdflatex lec2.tex
+
+cd ../lecture3
+pdflatex lec3.tex
+
+cd ../lecture4
+pdflatex lec4.tex
+
+cd ../lecture5
+pdflatex lec5.tex
+
+cd ../lecture6
+pdflatex lec6.tex
+
+cd ../lecture7
+pdflatex lec7.tex
+
+cd ../lecture8
+pdflatex lec8.tex
+
+cd ../lecture9
+pdflatex lec9.tex
+
+cd ../lecture10
+pdflatex lec10.tex
+
